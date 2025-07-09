a9dd3a6a6a3c4eb9189ae0c16621ae747a4e6b1e
codesworth <codezworth@gmail.com>
2025-07-09 05:57:58 +0000

    Initial Commit

---
+++ Snippet-Intents-WWDC25/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snippet-Intents-WWDC25
+//
+//  Created by Shadrach Mensah on 09/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snippet-Intents-WWDC25/Snippet_Intents_WWDC25App.swift
@@ -0,0 +1,17 @@
+//
+//  Snippet_Intents_WWDC25App.swift
+//  Snippet-Intents-WWDC25
+//
+//  Created by Shadrach Mensah on 09/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Snippet_Intents_WWDC25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
